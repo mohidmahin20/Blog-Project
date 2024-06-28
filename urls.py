0ff9a0b98952ee0ddad81e6efcab54c3d9094bd9
Mahin <email@example.com>
2024-06-28 16:19:53 +0600

    first commit

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    
+   # path('add/', views.add_post, name='add_post'),
+     path('', views.index, name='index'),  # Example view
+]
